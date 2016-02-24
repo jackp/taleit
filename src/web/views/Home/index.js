805af2470baf21ad9f20c8809c6413fbb98e32ef
Jackson Parker <parkej3@gmail.com>
2016-02-24 17:57:57 -0500

    [cleaning] Cleanup testing code

--- src/web/views/Home/index.js
+++ src/web/views/Home/index.js
@@ -3,30 +3,19 @@
  */
 
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
 
 import View from 'containers/View';
-import { login } from 'actions/user';
 import styles from './home.css';
 
-@connect(state => ({
-  user: state.user,
-}))
 class HomeView extends Component {
   static propTypes = {
     user: PropTypes.object,
-  };
-
-  handleClick = () => {
-    this.props.dispatch(login());
   };
 
   render() {
     return (
       <View className={styles.root}>
         Home View
-        { this.props.user ? `Hello ${this.props.user.name}` : null }
-        <button onClick={this.handleClick}>Login</button>
       </View>
     );
   }
