bf2535b37de825572bc3a1ce1764e58a58326326
Jackson Parker <parkej3@gmail.com>
2016-02-24 17:58:42 -0500

    More cleaning up testing code

--- src/web/views/Home/index.js
+++ src/web/views/Home/index.js
@@ -2,16 +2,12 @@
  * <HomeView />
  */
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
 import View from 'containers/View';
 import styles from './home.css';
 
 class HomeView extends Component {
-  static propTypes = {
-    user: PropTypes.object,
-  };
-
   render() {
     return (
       <View className={styles.root}>
