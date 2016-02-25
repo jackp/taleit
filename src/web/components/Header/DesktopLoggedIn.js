ba67f98c98f4a8474baadec501fc9bc39134c78d
Jackson Parker <parkej3@gmail.com>
2016-02-25 16:46:42 -0500

    Add header dropdown menu

--- src/web/components/Header/DesktopLoggedIn.js
+++ src/web/components/Header/DesktopLoggedIn.js
@@ -5,6 +5,7 @@
 import React, { Component, PropTypes } from 'react';
 import { IndexLink, Link } from 'react-router';
 
+import Icon from 'components/Icon';
 import logo from 'images/logo.svg';
 import styles from './header.css';
 
@@ -25,7 +26,12 @@ class DesktopLoggedIn extends Component {
         <Link to="discover" className={styles.primaryLink}>DISCOVER</Link>
 
         <div className={styles.rightMenu}>
-          <Link to={user.username}>{ user.name }</Link>
+          <Link to={user.username} className={styles.dropdownItem}>
+            { user.name }
+            <Icon name="caret-down" />
+          </Link>
+          <div className={styles.dropdownMenu}>
+          </div>
         </div>
       </div>
     );
