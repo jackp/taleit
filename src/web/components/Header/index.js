06cabcc3c97d787ed752d00a583877f69b48d17e
Jackson Parker <parkej3@gmail.com>
2016-02-25 22:42:59 -0500

    Use VelocityComponent for animations seems to solve all problems

--- src/web/components/Header/index.js
+++ src/web/components/Header/index.js
@@ -4,7 +4,7 @@
  */
 
 import React, { Component, PropTypes } from 'react';
-import { VelocityTransitionGroup } from 'velocity-react';
+import { VelocityComponent } from 'velocity-react';
 import { connect } from 'react-redux';
 import MediaQuery from 'react-responsive';
 
@@ -12,7 +12,6 @@ import DesktopLoggedIn from './DesktopLoggedIn';
 import DesktopLoggedOut from './DesktopLoggedOut';
 import MobileLoggedIn from './MobileLoggedIn';
 import MobileLoggedOut from './MobileLoggedOut';
-import animations from 'utils/animations';
 import styles from './header.css';
 
 const mobileBreakpoint = 968;
@@ -31,22 +30,16 @@ class Header extends Component {
     const { show, user } = this.props;
 
     return (
-      <VelocityTransitionGroup
-        enter={animations.slideDown}
-        leave={animations.slideUp}
-      >
-        { show ?
-          <header className={styles.root}>
-            <MediaQuery minWidth={mobileBreakpoint} values={{ width: mobileBreakpoint }}>
-              { user ? <DesktopLoggedIn user={user} /> : <DesktopLoggedOut /> }
-            </MediaQuery>
-            <MediaQuery maxWidth={mobileBreakpoint - 1}>
-              { user ? <MobileLoggedIn user={user} /> : <MobileLoggedOut /> }
-            </MediaQuery>
-          </header>
-          : null
-        }
-      </VelocityTransitionGroup>
+      <VelocityComponent animation={{ translateY: show ? 0 : '-100%' }}>
+        <header className={styles.root}>
+          <MediaQuery minWidth={mobileBreakpoint} values={{ width: mobileBreakpoint }}>
+            { user ? <DesktopLoggedIn user={user} /> : <DesktopLoggedOut /> }
+          </MediaQuery>
+          <MediaQuery maxWidth={mobileBreakpoint - 1}>
+            { user ? <MobileLoggedIn user={user} /> : <MobileLoggedOut /> }
+          </MediaQuery>
+        </header>
+      </VelocityComponent>
     );
   }
 }
